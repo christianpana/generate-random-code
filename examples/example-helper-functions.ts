c485ce21465c59db81eef9a6a505f2f973636318
Cristian <christianpanax@gmail.com>
2023-03-24 12:39:18 +0200

    chore: keep examples aligned with codesandbox examples

--- examples/example-helper-functions.ts
+++ examples/example-helper-functions.ts
@@ -7,32 +7,43 @@ import {
     generateCouponCode,
 } from '../src/index.js';
 
+console.log('-----------------------------')
+console.log('Helper functions');
+console.log('-----------------------------')
+console.log('\n')
+
 console.log('generatePinCode examples:');
 console.log(generatePinCode()); // Example output: "1249"
 console.log(generatePinCode()); // Example output: "3556"
 console.log(generatePinCode()); // Example output: "8343"
+console.log('\n')
 
 console.log('generateAccessCode examples:');
 console.log(generateAccessCode()); // Example output: "X42RPK"
 console.log(generateAccessCode()); // Example output: "73IDIG"
 console.log(generateAccessCode()); // Example output: "BN929F"
+console.log('\n')
 
 console.log('generateAuthCode examples:');
 console.log(generateAuthCode()); // Example output: "321204"
 console.log(generateAuthCode()); // Example output: "193928"
 console.log(generateAuthCode()); // Example output: "903528"
+console.log('\n')
 
 console.log('generateSecurityCode examples:');
 console.log(generateSecurityCode()); // Example output: "DYYs1V"
 console.log(generateSecurityCode()); // Example output: "6s5KPb"
 console.log(generateSecurityCode()); // Example output: "o1Q3XC"
+console.log('\n')
 
 console.log('generateVerificationCode examples:');
 console.log(generateVerificationCode()); // Example output: "76336"
 console.log(generateVerificationCode()); // Example output: "43633"
 console.log(generateVerificationCode()); // Example output: "67647"
+console.log('\n')
 
 console.log('generateCouponCode examples:');
 console.log(generateCouponCode()); // Example output: "LR68LIF1ES0XB96NM1DZ"
 console.log(generateCouponCode()); // Example output: "GGNOETNYXFWLXDZW1AS1"
 console.log(generateCouponCode()); // Example output: "0H8E3PCRI4E6DHNL9D35"
+console.log('\n')
--- examples/example-random-code.ts
+++ examples/example-random-code.ts
@@ -1,70 +1,83 @@
 import { randomCode, InfiniteLoopError} from '../src/index.js';
 
-console.log('Example 1:');
+console.log('-----------------------------')
+console.log('ramdomCode');
+console.log('-----------------------------')
+console.log('\n')
+
+console.log('randomCode example 1:');
 console.log(
     randomCode(8, {
         alphaNumericOnly: true,
         letterCasing: 'lowercase',
     })
 ); // Example output: "pdebif57"
+console.log('\n')
 
-console.log('Example 2:');
+console.log('randomCode example 2:');
 console.log(
     randomCode(6, {
         alphaOnly: true,
         letterCasing: 'mixed',
     })
 ); // Example output: "PAPPqb"
+console.log('\n')
 
-console.log('Example 3:');
+console.log('randomCode example 3:');
 console.log(
     randomCode(10, {
         charset: 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz123456789',
         excludeBadWords: true,
     })
 ); // Example output: "T59vsM3CNd"
+console.log('\n')
 
-console.log('Example 4:');
+console.log('randomCode example 4:');
 console.log(
     randomCode(6, {
         numericOnly: true,
         excludeSimilarChars: true,
     })
 ); // Example output: "246327"
+console.log('\n')
 
-console.log('Example 5:');
+console.log('randomCode example 5:');
 console.log(
     randomCode(8, {
         alphaNumericWithSymbols: true,
         letterCasing: 'uppercase',
     })
 ); // Example output: "?4(G]X*]"
+console.log('\n')
 
-console.log('Example 6:');
+console.log('randomCode example 6:');
 console.log(
     randomCode(12, {
         charset: 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz!@#$%&*',
         letterCasing: 'mixed',
     })
 ); // Example output: "FVUKHacFJtcK"
+console.log('\n')
 
-console.log('Example 7:');
+console.log('randomCode example 7:');
 console.log(
     randomCode(4, {
         alphaOnly: true,
         letterCasing: 'uppercase',
     })
 ); // Example output: "ZXXD"
+console.log('\n')
 
-console.log('Example 8:');
+console.log('randomCode example 8:');
 console.log(
     randomCode(7, {
         charset: 'ABCDEF0123456789',
         letterCasing: 'uppercase',
     })
 ); // Example output: "80E9355"
+console.log('\n')
 
-console.log('Example 9:');
+console.log('randomCode example 9:');
 console.log(
     randomCode(5, {
         excludeSimilarChars: true,
@@ -73,27 +86,30 @@ console.log(
         letterCasing: 'lowercase',
     })
 ); // Example output: "s7jdd"
+console.log('\n')
 
-console.log('Example 10:');
+console.log('randomCode example 10:');
 console.log(
     randomCode(10, {
         badWords: ["BADWORD1", "badword2"],
         excludeBadWords: true,
     })
 ); // Example output: "3iw5kywtaa"
+console.log('\n')
 
-console.log('Example 11:');
+console.log('randomCode example 11:');
 console.log(
     randomCode(20, {
         charset: 'HA',
         letterCasing: 'mixed',
     })
 ); // Example output: "hHHaahAHhHhHaahahhha" :)
+console.log('\n')
 
-console.log('Example 12:');
+console.log('randomCode example 12 - potential infinite loop:');
 try {
     console.log(
-        randomCode(30, {
+        randomCode(50, {
             charset: 'as',
             excludeBadWords: true,
             letterCasing: 'lowercase',
@@ -107,3 +123,4 @@ try {
     }
 }
 // Example output: We had a possible infinite loop
+console.log('\n')
