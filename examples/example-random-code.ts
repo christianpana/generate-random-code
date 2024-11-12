60c2d61044d9d8ad3556f679b8a8fd9f31c6b43f
Cristian <christianpanax@gmail.com>
2024-11-12 15:03:46 +0100

    updated examples

--- examples/example-random-code.ts
+++ examples/example-random-code.ts
@@ -1,126 +1,126 @@
-import { randomCode, InfiniteLoopError} from '../src/index.js';
+import randomCode, { InfiniteLoopError } from '../src';
 
-console.log('-----------------------------')
+console.log('-----------------------------');
 console.log('ramdomCode');
-console.log('-----------------------------')
-console.log('\n')
+console.log('-----------------------------');
+console.log('\n');
 
 console.log('randomCode example 1:');
 console.log(
-    randomCode(8, {
-        alphaNumericOnly: true,
-        letterCasing: 'lowercase',
-    })
+  randomCode(8, {
+    characterSet: 'alphanumeric',
+    letterCasing: 'lowercase',
+  }),
 ); // Example output: "pdebif57"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 2:');
 console.log(
-    randomCode(6, {
-        alphaOnly: true,
-        letterCasing: 'mixed',
-    })
+  randomCode(6, {
+    characterSet: 'alpha',
+    letterCasing: 'mixed',
+  }),
 ); // Example output: "PAPPqb"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 3:');
 console.log(
-    randomCode(10, {
-        charset: 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz123456789',
-        excludeBadWords: true,
-    })
+  randomCode(10, {
+    charset: 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz123456789',
+    excludeDefaultBadWords: true,
+  }),
 ); // Example output: "T59vsM3CNd"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 4:');
 console.log(
-    randomCode(6, {
-        numericOnly: true,
-        excludeSimilarChars: true,
-    })
+  randomCode(6, {
+    characterSet: 'numeric',
+    excludeDefaultSimilarChars: true,
+  }),
 ); // Example output: "246327"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 5:');
 console.log(
-    randomCode(8, {
-        alphaNumericWithSymbols: true,
-        letterCasing: 'uppercase',
-    })
+  randomCode(8, {
+    characterSet: 'alphanumericsymbols',
+    letterCasing: 'uppercase',
+  }),
 ); // Example output: "?4(G]X*]"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 6:');
 console.log(
-    randomCode(12, {
-        charset: 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz!@#$%&*',
-        letterCasing: 'mixed',
-    })
+  randomCode(12, {
+    charset: 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz!@#$%&*',
+  }),
 ); // Example output: "FVUKHacFJtcK"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 7:');
 console.log(
-    randomCode(4, {
-        alphaOnly: true,
-        letterCasing: 'uppercase',
-    })
+  randomCode(4, {
+    characterSet: 'alpha',
+    letterCasing: 'uppercase',
+  }),
 ); // Example output: "ZXXD"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 8:');
 console.log(
-    randomCode(7, {
-        charset: 'ABCDEF0123456789',
-        letterCasing: 'uppercase',
-    })
+  randomCode(7, {
+    charset: 'ABCDEF0123456789',
+  }),
 ); // Example output: "80E9355"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 9:');
 console.log(
-    randomCode(5, {
-        excludeSimilarChars: true,
-        excludeBadWords: true,
-        alphaNumericOnly: true,
-        letterCasing: 'lowercase',
-    })
+  randomCode(5, {
+    excludeDefaultSimilarChars: true,
+    excludeDefaultBadWords: true,
+    characterSet: 'alphanumeric',
+    letterCasing: 'lowercase',
+  }),
 ); // Example output: "s7jdd"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 10:');
 console.log(
-    randomCode(10, {
-        badWords: ["BADWORD1", "badword2"],
-        excludeBadWords: true,
-    })
+  randomCode(10, {
+    characterSet: 'alphanumeric',
+    excludeBadWords: ['BADWORD1', 'badword2'],
+  }),
 ); // Example output: "3iw5kywtaa"
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 11:');
 console.log(
-    randomCode(20, {
-        charset: 'HA',
-        letterCasing: 'mixed',
-    })
+  randomCode(20, {
+    charset: 'HAha',
+  }),
 ); // Example output: "hHHaahAHhHhHaahahhha" :)
-console.log('\n')
+console.log('\n');
 
 console.log('randomCode example 12 - potential infinite loop:');
 try {
-    console.log(
-        randomCode(50, {
-            charset: 'as',
-            excludeBadWords: true,
-            letterCasing: 'lowercase',
-        })
-    );
+  console.log(
+    randomCode(50, {
+      charset: 'as',
+      excludeDefaultBadWords: true,
+    }),
+  );
 } catch (err) {
-    if (err instanceof InfiniteLoopError) {
-        console.log('We had a possible infinite loop')
-    } else {
-        console.log('Something definitely went wrong ...')
-    }
+  if (err instanceof InfiniteLoopError) {
+    console.log('We had a possible infinite loop');
+  } else {
+    console.log('Something definitely went wrong ...');
+  }
 }
 // Example output: We had a possible infinite loop
-console.log('\n')
+console.log('\n');
+
+console.log('randomCode example 13:');
+console.log(randomCode()); // Example output: "fGxjPbwG"
+console.log('\n');
